43fbd9995dd8d8ea449d97d7a3e36482c7f2c9dd
Yann Beaud <yann.beaud@gmail.com>
2014-08-13 15:04:54 +0200

    Update fields max lenght to 4000

--- mezzanine/forms/models.py
+++ mezzanine/forms/models.py
@@ -75,7 +75,7 @@ class Field(Orderable):
     field_type = models.IntegerField(_("Type"), choices=fields.NAMES)
     required = models.BooleanField(_("Required"), default=True)
     visible = models.BooleanField(_("Visible"), default=True)
-    choices = models.CharField(_("Choices"), max_length=1000, blank=True,
+    choices = models.CharField(_("Choices"), max_length=4000, blank=True,
         help_text=_("Comma separated options where applicable. If an option "
             "itself contains commas, surround the option with `backticks`."))
     default = models.CharField(_("Default value"), blank=True,
