d932c14943d773b82d7eee5fb617102e205fd1c4
Yann Beaud <yann.beaud@gmail.com>
2014-08-27 01:44:19 +0200

    Fix typo

--- mezzanine/forms/models.py
+++ mezzanine/forms/models.py
@@ -176,4 +176,4 @@ class Payment(models.Model):
     def reference(self):
         """Return a reference for the payment"""
 
-        return "mezzanine-payment-%s-(Form-ID-%s)" % (self.pk, self.entry.form.pk)
+        return "mezzanine-payment-%s-(Form-ID-%s)" % (self.pk, self.entry.form.pk, )
